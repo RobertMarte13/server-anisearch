a8c4772991e457753458be08154a80330b34b4e9
RobertMarte13 <rproject133gmail.com>
2023-07-14 14:40:09 -0400

    Agregando un id para eliminar o cancelar el like.

--- src/routes/comments_likes.routes.js
+++ src/routes/comments_likes.routes.js
@@ -6,11 +6,11 @@ const routes = Router()
 // Crear likes
 routes.post('/api/comments_likes', async (req, res) => {
     // Este codigo recibe el user_id o auth_id, el delete_id y el update_likes unico que servira para dar o quitar likes.
-    const { users_id, comments_id } = req.body;
+    const { users_id, comments_id, id_delete } = req.body;
     try {
 
         // Aqui le estamos indicando que cree en la base de datos con lo siguientes parametros un registro de los likes.
-        await pool.query("INSERT INTO comment_likes(users_id, comments_id) VALUES (?, ?)", [users_id, comments_id]);
+        await pool.query("INSERT INTO comment_likes(users_id, comments_id, id_delete) VALUES (?, ?, ?)", [users_id, comments_id, id_delete]);
 
         // Message que aparece como protocolo de que si se iso el like
         res.status(201).json({message: "success"})
@@ -26,8 +26,11 @@ routes.get('/api/comments_likes', async (req, res) => {
         // Esta peticion a la base de datos es para que muestre los likes que se an dado y que tambien mande los id de los likes para comparar y buscar a que comentario pertenece.
         const [rows] = await pool.query("SELECT comments_id, COUNT(comments_id) AS like_count FROM comment_likes GROUP BY comments_id");
 
+        const [rows_2] = await pool.query("SELECT * FROM comment_likes")
+
         res.json({
-            data: rows
+            data: rows,
+            delete_data: rows_2
         })
     } catch (error) {
         res.json({
